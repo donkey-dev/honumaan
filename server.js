234ca5f0ac3d87bdcc71d866fbb081c387859ac3
arkajyotibose003 <arkajyotibose003@gmail.com>
2025-07-08 09:57:36 +0530

    All files

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" folder
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "main.html"));
+});
+
+app.get('/card', (req, res) => {
+    res.sendFile(path.join(__dirname, "card.html"));
+});
+
+// 🔹 START SERVER
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
